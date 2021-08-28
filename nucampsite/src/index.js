269fea63859c5f4639271ebe80a9806cb38681af
BennyD <powerbenny1@gmail.com>
2021-08-28 00:34:56 -0400

    Components Part 2

--- nucampsite/src/index.js
+++ nucampsite/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
